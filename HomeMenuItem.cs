4c6e55d74b482e5de2c65bd1230f9530bf516c16
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-12-17 18:34:13 +0800

    Add files via upload

---
+++ HomeMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CustomMasterDetailTest.Model.SliderView
+{
+	public enum MenuType
+	{
+		DashBoard,
+		Profile,
+		Test,
+	}
+	public class HomeMenuItem : BaseModel
+	{
+		public HomeMenuItem()
+		{
+			MenuType = MenuType.DashBoard;
+		}
+		public string Icon { get; set; }
+		public MenuType MenuType { get; set; }
+	}
+}
