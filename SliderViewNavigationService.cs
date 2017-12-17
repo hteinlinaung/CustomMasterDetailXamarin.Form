efdc12e1571b2a2ca0365ab404ecd5cdf9d0b466
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-12-17 18:33:56 +0800

    Add files via upload

---
+++ SliderViewNavigationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CustomMasterDetailTest.Model
+{
+	public class SliderViewNavigationService : NavigationPage
+	{
+		public SliderViewNavigationService(Page root) : base(root)
+		{
+			Init();
+		}
+
+		public SliderViewNavigationService()
+		{
+			Init();
+		}
+
+		void Init()
+		{
+
+			BarBackgroundColor = Color.FromHex("#03A9F4");
+			BarTextColor = Color.White;
+		}
+	}
+}
