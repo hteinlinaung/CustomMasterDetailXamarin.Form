2dbb51748e0d44c9ea0e9ed43b4963630a97cc2d
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-12-17 18:33:16 +0800

    Add files via upload

---
+++ ListViewPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace CustomMasterDetailTest
+{
+    public partial class ListViewPage : ContentPage
+    {
+		public ListViewPage(int id)
+		{
+			InitializeComponent();
+
+            if(id==1)
+            {
+
+                this.BindingContext = new[] { "a", "b", "c" };
+            }
+			else if (id == 2)
+			{
+
+				this.BindingContext = new[] { "shwe", "e", "f" };
+			}
+			else if (id == 3)
+			{
+
+				this.BindingContext = new[] { "j", "r", "Neymar" };
+			}
+			
+		}
+
+		void OnItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			if (e == null) return; // has been set to null, do not 'process' tapped event
+			Debug.WriteLine("Tapped: " + e.Item);
+			((ListView)sender).SelectedItem = null; // de-select the row
+		}
+    }
+}
---
+++ MenuPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using CustomMasterDetailTest.Model.SliderView;
+using Xamarin.Forms;
+
+namespace CustomMasterDetailTest
+{
+	public partial class MenuPage : ContentPage
+	{
+		RootPage root;
+		List<HomeMenuItem> menuItems;
+		public MenuPage(RootPage root)
+		{
+
+			this.root = root;
+			InitializeComponent();
+
+			BackgroundColor = Color.FromHex("#03A9F4");
+			ListViewMenu.BackgroundColor = Color.FromHex("#F5F5F5");
+
+			BindingContext = new BaseViewModel
+			{
+				Title = "MatchHelper",
+				Subtitle = "MatchHelper",
+				Icon = "slideout.png"
+			};
+
+			ListViewMenu.ItemsSource = menuItems = new List<HomeMenuItem>
+				{
+					new HomeMenuItem { Title = "Dashboard", MenuType = MenuType.DashBoard, Icon ="about.png" },
+					new HomeMenuItem { Title = "Profile", MenuType = MenuType.Profile, Icon = "blog.png" },
+					new HomeMenuItem { Title = "Test", MenuType = MenuType.Test, Icon = "twitternav.png" },
+
+
+				};
+
+			ListViewMenu.SelectedItem = menuItems[0];
+
+			ListViewMenu.ItemSelected += async (sender, e) =>
+			{
+				if (ListViewMenu.SelectedItem == null)
+					return;
+
+				await this.root.NavigateAsync(((HomeMenuItem)e.SelectedItem).MenuType);
+			};
+		}
+	}
+}
